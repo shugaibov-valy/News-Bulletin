b618d7e9ce9e3c8149f6dc31907fae43ba74a83a
shugaibov-valy <dorgeli2019@gmail.com>
2021-01-01 07:48:05 +0000

    commit style_css

---
+++ style.py
@@ -0,0 +1,45 @@
+style_css = '''               
+    QFrame#horizontalFrame {
+        background-color: rgb(0, 0, 255);   
+    }
+
+    QLabel#label{
+        color: white;
+    }
+
+    QLabel#label_2{
+        font-size: 15px;
+        font-weight: bold;
+    }
+
+    QComboBox {
+        background-color: blue;
+        font-size: 17px;
+        color: white;
+    }
+
+    QPushButton#pushButton {
+        background-color: blue;
+        color: white;
+        font-size: 15px;
+    }
+
+    QPushButton#pushButton_2 {
+        font-size: 15px;
+        font-weight: bold;
+    }
+
+    QTextEdit{
+        font-size: 20px;    
+    }
+
+    QListWidget {
+        background-color: rgb(204, 0, 2);
+        color: white;
+        font-size: 18px;
+    }
+
+    QLabel#label_3 {
+        background-image: url(soc.PNG);
+    }
+'''
